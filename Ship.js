882fd2426b36fc3af80a5fdabbcad1a39e489bb7
Nadya Dolgoruchenko <dolgoruchenko1999@ya.ru>
2026-08-19 12:14:56 +0300

    Add new version of class Ship

---
+++ Ship.js
@@ -0,0 +1,34 @@
+export class Ship {
+	constructor(size, positioning) {
+		this._size = size;
+		this._isVertical = positioning;
+		this._hits = new Array(size).fill(false);
+	}
+
+	get size() {
+		return this._size;
+	}
+
+	get hits() {
+		return this._hits;
+	}
+
+	set hits(hits) {
+		this._hits = hits;
+	}
+
+	get isVertical() {
+		return this._isVertical;
+	}
+
+	hit(index) {
+		if (index >= 0 && index < this.size) {
+			this._hits[index] = true;
+		}
+		return this._hits[index];
+	}
+
+	isSunk() {
+		return this._hits.every((hit) => hit === true);
+	}
+}
